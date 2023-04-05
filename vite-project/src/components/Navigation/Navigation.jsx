cd80f443375c36dda06c50208d49d6dff911858f
natali ghibradze <115106231+natalighibradze@users.noreply.github.com>
2023-04-06 01:34:17 +0400

    Update Navigation.jsx

--- vite-project/src/components/Navigation/Navigation.jsx
+++ vite-project/src/components/Navigation/Navigation.jsx
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button, Typography } from '@mui/material';
-import axios from 'axios';
 
+import jwtDecode from 'jwt-decode';
 import './Navigation.scss';
 import { useStore } from '../../store/StoreContext';
+
+
 
 const Navigation = () => {
   const { productQuantity: cartSize, handleChangeTheme } = useStore();
 
-  const [searchTerm, setSearchTerm] = useState("");
+  const userToken = localStorage.getItem("token");
+  const user = jwtDecode(userToken);
+
   const navigate = useNavigate();
   const handleLogOut = () => {
     localStorage.removeItem('token');
     navigate('/login');
   };
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const searchResult = await axios.get(`https://dummyjson.com/products/search?q=${searchTerm}`);
-    navigate(`/search/${searchTerm}`, { searchResult });
-  };
+  
+
 
   return (
     <header className="container">
       <nav className="subcontainer">
+        
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -32,25 +34,23 @@ const Navigation = () => {
           <li>
             <Link to="/cart">Cart {cartSize}</Link>
           </li>
+
+          <li>
+            <Link to={'/profile'}>
+               {user.username}
+            </Link>
+          </li>
         </ul>
-        <form onSubmit={handleSubmit}>
-          <input 
-            type="text"
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-          />
-          <Button type="submit" variant="contained" color="error" >
-            Search
-          </Button>
-        </form>
         <Button variant="contained" color="error" onClick={handleLogOut}>
           Log out
         </Button>
         <Typography variant="subtitle1" color="white" onClick={handleChangeTheme}>
           Change Theme
         </Typography>
+        
       </nav>
     </header>
   );
 };
 export default Navigation;
+// export default UsersPage;
