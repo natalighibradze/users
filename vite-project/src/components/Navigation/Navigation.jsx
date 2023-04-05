bbf7270fb9bc1068118a5dc12172242ce2fbaf49
natali ghibradze <115106231+natalighibradze@users.noreply.github.com>
2023-04-06 00:24:39 +0400

    Update Navigation.jsx

--- vite-project/src/components/Navigation/Navigation.jsx
+++ vite-project/src/components/Navigation/Navigation.jsx
@@ -1,51 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { Button, Typography } from '@mui/material';
+import axios from 'axios';
 
 import './Navigation.scss';
 import { useStore } from '../../store/StoreContext';
 
-import { Avatar } from '@mui/material';
-
 const Navigation = () => {
   const { productQuantity: cartSize, handleChangeTheme } = useStore();
-  const userInfo = {
-    "id": 15,
-    "username": "kminchelle",
-    "email": "kminchelle@qq.com",
-    "firstName": "Jeanne",
-    "lastName": "Halvorson",
-    "gender": "female",
-    "image": "https://robohash.org/autquiaut.png?size=50x50&set=set1"
-  }
 
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
   const handleLogOut = () => {
     localStorage.removeItem('token');
     navigate('/login');
   };
 
-  const handleUserPage = () => {
-    navigate(`/user/${userInfo.id}`);
-  }
-
-  const UsersPage = () => {
-    return(
-      <div>
-        <h1>User Page</h1>
-        <p>Username: {userInfo.username}</p>
-        <p>Email: {userInfo.email}</p>
-        <p>First Name: {userInfo.firstName}</p>
-        <p>Last Name: {userInfo.lastName}</p>
-        <p>Gender: {userInfo.gender}</p>
-      </div>
-    )
-  }
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const searchResult = await axios.get(`https://dummyjson.com/products/search?q=${searchTerm}`);
+    navigate(`/search/${searchTerm}`, { searchResult });
+  };
 
   return (
     <header className="container">
       <nav className="subcontainer">
-        
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -54,17 +33,24 @@ const Navigation = () => {
             <Link to="/cart">Cart {cartSize}</Link>
           </li>
         </ul>
-        <Avatar src={userInfo.image} onClick={handleUserPage}/>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+          <Button type="submit" variant="contained" color="error" >
+            Search
+          </Button>
+        </form>
         <Button variant="contained" color="error" onClick={handleLogOut}>
           Log out
         </Button>
         <Typography variant="subtitle1" color="white" onClick={handleChangeTheme}>
           Change Theme
         </Typography>
-        
       </nav>
     </header>
   );
 };
 export default Navigation;
-// export default UsersPage;
